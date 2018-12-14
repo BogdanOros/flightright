6c3b1c22e61f807db6fb057f9181bde27e3bf977
BogdanOros <orossbogdan@gmail.com>
2018-12-14 14:32:15 +0200

    Extracted primary port (Member service), removed unnecessary annotations

--- src/main/java/io/boros/flightright/member/MemberService.java
+++ src/main/java/io/boros/flightright/member/MemberService.java
@@ -1,41 +1,20 @@
 package io.boros.flightright.member;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@Service
-@RequiredArgsConstructor
-public class MemberService {
+public interface MemberService {
 
-    private final MemberRepository memberRepository;
+    Page<Member> getMembers(Pageable pageable);
 
-    public Page<Member> getMembers(Pageable pageable) {
-        return memberRepository.findAll(pageable);
-    }
+    Optional<Member> getMember(String id);
 
-    public Optional<Member> getMember(String id) {
-        return memberRepository.findById(id);
-    }
+    Member createMember(Member member);
 
-    public Member createMember(Member member) {
-        return memberRepository.save(member);
-    }
+    Optional<Member> updateMember(String id, Member member);
 
-    public Optional<Member> updateMember(String id, Member member) {
-        return memberRepository.findById(id)
-                .map(existing -> merge(member, existing));
-    }
-
-    private Member merge(Member from, Member to) {
-        to.setFirstName(from.getFirstName());
-        to.setLastName(from.getLastName());
-        to.setDateOfBirth(from.getDateOfBirth());
-        to.setZipCode(from.getZipCode());
-        return to;
-    }
+    void deleteMember(String id);
 
 }
