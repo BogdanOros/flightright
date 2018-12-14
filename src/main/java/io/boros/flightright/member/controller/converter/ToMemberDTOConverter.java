bfa68c5248d19ef7353432d5b7957e53bd2caebe
BogdanOros <orossbogdan@gmail.com>
2018-12-15 01:02:49 +0200

    Storing image name instead of URL.

--- src/main/java/io/boros/flightright/member/controller/converter/ToMemberDTOConverter.java
+++ src/main/java/io/boros/flightright/member/controller/converter/ToMemberDTOConverter.java
@@ -1,12 +1,17 @@
 package io.boros.flightright.member.controller.converter;
 
+import io.boros.flightright.image.ImageLocationProvider;
 import io.boros.flightright.member.model.Member;
 import io.boros.flightright.member.controller.MemberDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class ToMemberDTOConverter implements Converter<Member, MemberDTO> {
+
+    private final ImageLocationProvider locationProvider;
 
     @Override
     public MemberDTO convert(Member source) {
@@ -16,7 +21,7 @@ public class ToMemberDTOConverter implements Converter<Member, MemberDTO> {
                 source.getLastName(),
                 source.getZipCode(),
                 source.getDateOfBirth(),
-                source.getImage()
+                locationProvider.getURL(source.getImage())
         );
     }
 }
